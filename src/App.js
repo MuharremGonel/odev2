3c727da28b7b5d0542ef6875fed43a32cfc3dd72
HelloWord0101 <muharremgonel20@gmail.com>
2022-06-04 21:32:51 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import Form from './compenents/Form'
+import TodoList from  './compenents/TodoList'
+import Footer from './compenents/Footer'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(()=>{
+    const filterHandler = ()=>{
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))
+          break;
+        case 'active':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))
+          break;
+        default:
+          setFilteredTodos(todos)
+          break;
+      }
+    }
+
+    filterHandler()
+  }, [todos, status])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <Form todos={todos} setTodos={setTodos} todo={todos.todo}/>
+
+      <ul className='todo-list'>
+        {filteredTodos.map((todo)=>(
+          <TodoList
+          status={status}
+          key={todo.id}
+          todo={todo}
+          text={todo.text}
+          todos={todos}
+          setTodos={setTodos}
+          />
+        ))}
+      </ul>
+      
+
+      <Footer
+      status={status}
+      setStatus={setStatus}
+      todos={todos}
+      setTodos={setTodos}
+      />
     </div>
   );
 }
